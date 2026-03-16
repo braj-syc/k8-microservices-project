f6f964394507ef2b160323a50a9643a5d969f010
braj-syc <63956838+braj-syc@users.noreply.github.com>
2026-03-16 19:24:18 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,17 @@
+const API_URL = "/api";
 
+function getUsers() {
+  fetch(`${API_URL}/users`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").innerText = JSON.stringify(data, null, 2);
+    });
+}
+
+function getProducts() {
+  fetch(`${API_URL}/products`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").innerText = JSON.stringify(data, null, 2);
+    });
+}
