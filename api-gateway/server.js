e038a0ab9712ef225b34742a75d9c990acccb9d0
braj-syc <63956838+braj-syc@users.noreply.github.com>
2026-03-16 19:26:41 +0530

    Update server.js

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
 
+const USER_SERVICE = "http://user-service";
+const PRODUCT_SERVICE = "http://product-service";
+
+app.get("/users", async (req, res) => {
+  const response = await axios.get(USER_SERVICE);
+  res.json(response.data);
+});
+
+app.get("/products", async (req, res) => {
+  const response = await axios.get(PRODUCT_SERVICE);
+  res.json(response.data);
+});
+
+app.listen(3000, () => {
+  console.log("API Gateway running on port 3000");
+});
