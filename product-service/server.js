20c642c7ea508b82d04b129ceb658d58a00638b9
braj-syc <63956838+braj-syc@users.noreply.github.com>
2026-03-16 19:41:09 +0530

    Update server.js

--- product-service/server.js
+++ product-service/server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.json([
+    { id: 101, product: "Laptop" },
+    { id: 102, product: "Phone" }
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("Product service running on port 3000");
+});
